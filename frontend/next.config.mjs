3a12bf480398c5223e99e1e609b06c7921c4a5a2
Tomas <tomas@trybulk.cl>
2026-01-20 10:21:50 -0300

    feat: northflank deploy files

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'standalone', // Necesario para deployment con Docker
 };
 
 export default nextConfig;
